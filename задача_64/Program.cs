b73512e612f8f44e64acedb35d084504e17395da
Artem Antonenko <a-.-@MacBook-Pro-Artem.local>
2023-03-01 10:30:51 +0300

    first commit

---
+++ задача_64/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.WriteLine("введите n");
+int n = int.Parse(Console.ReadLine());
+PrintNumbers(n);
+
+
+string PrintNumbers(int number)
+{       if(number==1){
+            Console.WriteLine($"{number}");
+            return number.ToString();
+        }
+        string s = number.ToString()+ " " + PrintNumbers(number - 1);
+        Console.WriteLine($"{s}");
+        return s; }
+    
+
+
